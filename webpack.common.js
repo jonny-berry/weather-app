8e36001557b19dae312b898c44c9b7ecac8110d0
Jonny Berry <jonnyfps69@gmail.com>
2026-08-05 13:22:42 -0700

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+entry: {
+  index: "./src/index.js",
+},
+output: {
+  filename: "[name].js",
+  path: path.resolve(import.meta.dirname, "dist"),
+  clean: true,
+},
+plugins: [
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    filename: "index.html",
+    chunks: ["index"],
+  }),
+],
+module: {
+  rules: [
+  {
+    test: /\.css$/i,
+    use: ["style-loader", "css-loader"],
+  },
+  {
+    test: /\.html$/i,
+    use: ["html-loader"],
+  },
+  {
+    test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+    type: "asset/resource",
+  },
+  ],
+},
+};
