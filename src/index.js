5c199b68ab37cbb7ba41061fd9a8c355133b235d
Jonny <jonnydevvs@gmail.com>
2026-08-07 15:48:15 -0700

    feat: fetch weather data from visual crossing API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+async function fetchWeatherData(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=5LH3P3XXFNEJXGXWFJS9PWR5M`,
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
