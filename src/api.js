22af162da5ea9384aded8b1ca6e1d4bb00457e5b
Jonny <jonnydevvs@gmail.com>
2026-09-10 15:00:11 -0700

    refactor: separate api logic into its own module

---
+++ src/api.js
@@ -0,0 +1,35 @@
+async function fetchWeatherData(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=5LH3P3XXFNEJXGXWFJS9PWR5M`,
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export let weatherData = await fetchWeatherData("Paris");
+
+export function parseWeatherData(weatherData) {
+  const address = weatherData.address;
+  const temp = weatherData.currentConditions.temp;
+  const condition = weatherData.currentConditions.conditions;
+  const date = weatherData.days[0].datetime;
+  const time = weatherData.currentConditions.datetime;
+  const description = weatherData.description;
+  const hourlyTemps = weatherData.days[0].hours;
+  const dailyTemps = weatherData.days;
+
+  return {
+    address,
+    temp,
+    condition,
+    date,
+    time,
+    description,
+    hourlyTemps,
+    dailyTemps,
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,42 +1,6 @@
-import "./styles.css"
-import { renderWeatherPage } from "./render.js"
+import "./styles.css";
+import { renderWeatherPage } from "./render.js";
+import { parseWeatherData, weatherData } from "./api.js";
 
-async function fetchWeatherData(location) {
-  try {
-    const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=5LH3P3XXFNEJXGXWFJS9PWR5M`,
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-let data = await fetchWeatherData("Paris");
-
-function parseWeatherData(weatherData) {
-  const address = weatherData.address;
-  const temp = weatherData.currentConditions.temp;
-  const condition = weatherData.currentConditions.conditions;
-  const date = weatherData.days[0].datetime;
-  const time = weatherData.currentConditions.datetime;
-  const description = weatherData.description;
-  const hourlyTemps = weatherData.days[0].hours;
-  const dailyTemps = weatherData.days;
-
-  return {
-    address,
-    temp,
-    condition,
-    date,
-    time,
-    description,
-    hourlyTemps,
-    dailyTemps,
-  };
-}
-
-parseWeatherData(data);
-
+parseWeatherData(weatherData);
 renderWeatherPage();
